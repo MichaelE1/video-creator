ca2947c49c73eca7d2e41ab9977db737f3604e60
MichaelE1 <mickel13@gmail.com>
2018-10-15 17:40:01 +1000

    everything

--- app.js
+++ app.js
@@ -1,20 +1,58 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const ffmpeg = require('fluent-ffmpeg');
+const fileUpload = require('express-fileupload');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
+app.use(fileUpload());
 
-app.get('/', (req, res) => {
+app.post('/upload', function(req, res) {
+  if (Object.keys(req.files).length < 2) {
+    return res.status(400).send('Make sure you select an image and sound file. No files were uploaded :(');
+  }
 
-  // Test creating a video
-  ffmpeg()
-    .input('image.png')
-    .input('sound.mp3')
-    .save('video.mp4')
-  
+  // the name of the input field is used to retrieve the uploaded file
+  let imgFile = req.files.imgFile;
+  let soundFile = req.files.soundFile;
+
+  // store files on the server
+  imgFile.mv(`./public/${imgFile.name}`, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+  });
+
+  soundFile.mv(`./public/${soundFile.name}`, async (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    const createVideo = new Promise((resolve, reject) => {
+      ffmpeg()
+        .input(`./public/${imgFile.name}`)
+        .input(`./public/${soundFile.name}`)
+        .on('end', () => {
+          resolve();
+        })
+        .save('./public/video.mp4')
+    });
+
+    await createVideo;
+    res.render('video')
+  });
+});
+
+app.get('/', (req, res) => {  
   res.render('index');
+});
+
+app.get('/video', (req, res) => {
+  res.render('video');
 });
 
 app.listen(3000, () => {
   console.log('Running on port 3000')
-})
+});
